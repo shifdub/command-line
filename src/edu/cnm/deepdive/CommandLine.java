0993fdf1db5ab3fb7c630ddf088b669ecfd6a850
isaac <shifdub@gmail.com>
2020-09-22 09:29:55 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/CommandLine.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive;
+
+public class CommandLine {
+
+  public static void main(String[] args) {
+    for (int i = 0; i <args.length; i++) {
+      System.out.println(args[i].toUpperCase());
+      }
+      System.out.println("Done");
+    }
+
+  }
